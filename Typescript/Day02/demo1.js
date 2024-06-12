26f8d30765b2b7e8738ecb2c31b93eebf5532f24
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2024-06-12 21:51:19 +0530

    Day02

---
+++ Typescript/Day02/demo1.js
@@ -0,0 +1,30 @@
+// console.log("Hello World");
+// console.log('Welcome to Angular Module')
+// console.log(`
+//         Angular 16 By Sumit Raokhande
+//     `);
+/** */
+//variables
+var a1;
+var a_1, a$1;
+//data type 
+// number(int,long,short,double,float),string(''/""),boolean(true/false)
+// any => number,string,boolean
+// void => it is neutral data 
+// misec null & undefined => it acts as data type as well as value 
+var a = 10; // forward declration & defination 
+// console.log("Value of a is "+a);
+// console.log(`
+//         Value of a ${a}
+//     `);
+// var str!:string;
+// console.log("Value of str is "+str);
+// str="Sumit";
+// console.log("Value of str is "+str);
+var c;
+c = 2.5;
+console.log("Value of c is " + c);
+c = 'Sumit';
+console.log("Value of c is " + c);
+c = true;
+console.log("Value of c is " + c);
---
+++ Typescript/Day02/demo1.ts
@@ -0,0 +1,49 @@
+// console.log("Hello World");
+// console.log('Welcome to Angular Module')
+// console.log(`
+//         Angular 16 By Sumit Raokhande
+//     `);
+/** */
+
+//variables
+var a1;
+var a_1,a$1;
+//data type 
+// number(int,long,short,double,float),string(''/""),boolean(true/false)
+// any => number,string,boolean
+// void => it is neutral data 
+// misec null & undefined => it acts as data type as well as value 
+
+var a:number=10   // forward declration & defination 
+
+// console.log("Value of a is "+a);
+// console.log(`
+//         Value of a ${a}
+//     `);
+
+// var str!:string;
+// console.log("Value of str is "+str);
+// str="Sumit";
+// console.log("Value of str is "+str);
+
+// var c:any;
+// c=2.5;
+// console.log("Value of c is "+c);
+// c='Sumit';
+// console.log("Value of c is "+c);
+// c=true;
+// console.log("Value of c is "+c);
+
+
+
+//Literal
+
+var c1:number|string|undefined;
+c1=20;
+c1='Sumit';
+// c1=true;
+
+var c2:52|string|null;
+c2=52;
+c2='sumit';
+
